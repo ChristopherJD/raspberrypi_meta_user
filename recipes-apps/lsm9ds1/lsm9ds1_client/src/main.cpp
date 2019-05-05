6a3d3dd52b7be0e144bf7ebdf62d16a9c9d913a5
ChristopherJD <jordan.denny5@gmail.com>
2019-05-05 17:44:28 -0500

    WIP Adding client program for lsm9ds1. Still need to fix issues sending buffer over.

---
+++ recipes-apps/lsm9ds1/lsm9ds1_client/src/main.cpp
@@ -0,0 +1,41 @@
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+enum { max_length = 1024 };
+
+int main(int argc, char* argv[])
+{
+  try
+  {
+    if (argc != 3)
+    {
+      std::cerr << "Usage: blocking_tcp_echo_client <host> <port>\n";
+      return 1;
+    }
+
+    boost::asio::io_service io_service;
+
+    tcp::resolver resolver(io_service);
+    tcp::resolver::query query(tcp::v4(), argv[1], argv[2]);
+    tcp::resolver::iterator iterator = resolver.resolve(query);
+
+    tcp::socket s(io_service);
+    boost::asio::connect(s, iterator);
+
+    using namespace std; // For strlen.
+    uint8_t read_buffer[1] = {0};
+    size_t reply_length = boost::asio::read(s,
+        boost::asio::buffer(read_buffer, sizeof(read_buffer)));
+    std::cout << "Who: " << hex << read_buffer[0] << std::endl;
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
+}
--- recipes-apps/lsm9ds1/lsm9ds1_server/src/lsm9ds1_server.cpp
+++ recipes-apps/lsm9ds1/lsm9ds1_server/src/lsm9ds1_server.cpp
@@ -29,19 +29,157 @@
 #include <boost/asio.hpp>
 #include "lsm9ds1.h"
 
-int main(void) {
 
-	lsm9ds1_status_t ret = LSM9DS1_UNKNOWN_ERROR;	// Function return codes
+#include <cstdlib>
+#include <iostream>
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
 
-	ret = lsm9ds1_init(LSM9DS1_SPI_BUS);
-	if(ret < 0) {
-		return ret;
-	}
+using boost::asio::ip::tcp;
 
-	uint8_t read_buffer = 0;
-	lsm9ds1_read(0xF, &read_buffer);
+class session
+{
+public:
+  session(boost::asio::io_service& io_service)
+    : socket_(io_service)
+  {
+  }
 
-	printf("0x%x\n", read_buffer);
+  tcp::socket& socket()
+  {
+    return socket_;
+  }
 
-	return ret;
+  void start()
+  {
+    socket_.async_read_some(boost::asio::buffer(data_, max_length),
+        boost::bind(&session::handle_read, this,
+          boost::asio::placeholders::error,
+          boost::asio::placeholders::bytes_transferred));
+  }
+
+private:
+  void handle_read(const boost::system::error_code& error,
+      size_t bytes_transferred)
+  {
+    if (!error)
+    {
+      boost::asio::async_write(socket_,
+          boost::asio::buffer(data_, bytes_transferred),
+          boost::bind(&session::handle_write, this,
+            boost::asio::placeholders::error));
+    }
+    else
+    {
+      delete this;
+    }
+  }
+
+  void handle_write(const boost::system::error_code& error)
+  {
+    if (!error)
+    {
+    		uint8_t read_buffer = 0;
+    		lsm9ds1_read(0xF, &read_buffer);
+      socket_.async_read_some(boost::asio::buffer(&read_buffer, sizeof(read_buffer)),
+          boost::bind(&session::handle_read, this,
+            boost::asio::placeholders::error,
+            boost::asio::placeholders::bytes_transferred));
+    }
+    else
+    {
+      delete this;
+    }
+  }
+
+  tcp::socket socket_;
+  enum { max_length = 1024 };
+  char data_[max_length];
+};
+
+class server
+{
+public:
+  server(boost::asio::io_service& io_service, short port)
+    : io_service_(io_service),
+      acceptor_(io_service, tcp::endpoint(tcp::v4(), port))
+  {
+    start_accept();
+  }
+
+private:
+  void start_accept()
+  {
+    session* new_session = new session(io_service_);
+    acceptor_.async_accept(new_session->socket(),
+        boost::bind(&server::handle_accept, this, new_session,
+          boost::asio::placeholders::error));
+  }
+
+  void handle_accept(session* new_session,
+      const boost::system::error_code& error)
+  {
+    if (!error)
+    {
+      new_session->start();
+    }
+    else
+    {
+      delete new_session;
+    }
+
+    start_accept();
+  }
+
+  boost::asio::io_service& io_service_;
+  tcp::acceptor acceptor_;
+};
+
+int main(int argc, char* argv[])
+{
+	int32_t ret = -1;
+
+		ret = lsm9ds1_init(LSM9DS1_SPI_BUS);
+		if(ret < 0) {
+			return ret;
+		}
+
+  try
+  {
+    if (argc != 2)
+    {
+      std::cerr << "Usage: async_tcp_echo_server <port>\n";
+      return 1;
+    }
+
+    boost::asio::io_service io_service;
+
+    using namespace std; // For atoi.
+    server s(io_service, atoi(argv[1]));
+
+    io_service.run();
+  }
+  catch (std::exception& e)
+  {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
 }
+
+//int main(void) {
+//
+//	lsm9ds1_status_t ret = LSM9DS1_UNKNOWN_ERROR;	// Function return codes
+//
+//	ret = lsm9ds1_init(LSM9DS1_SPI_BUS);
+//	if(ret < 0) {
+//		return ret;
+//	}
+//
+//	uint8_t read_buffer = 0;
+//	lsm9ds1_read(0xF, &read_buffer);
+//
+//	printf("0x%x\n", read_buffer);
+//
+//	return ret;
+//}
