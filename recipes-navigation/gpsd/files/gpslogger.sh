e9c05fcddd8f869fdde614a77c64daac644e3281
Christopher Jordan-Denny <jordan.denny5@gmail.com>
2019-08-20 22:28:25 -0500

    Updated Log Management
    Updated the log management for the gps log. 10 logs max. The gps log if
    it exists, is always rotated during power up and restart.

--- recipes-navigation/gpsd/files/gpslogger.sh
+++ recipes-navigation/gpsd/files/gpslogger.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
-# First check if a past log exists. If it does, save it.
-if [ -f /var/log/gps.json ]; then
-	gzip /var/log/gps.json
-	mv /var/log/gps.json.gz /var/log/gps.json".$(date +"%Y%m%d-%H%M%S").gz"
+log_path="/var/log/"
+original_log_file=${log_path}"gps.json"
+
+if [ -f "${original_log_file}" ]; then
+
+	# Read the maximum number of logs from the config
+	number_of_logs=$(cat /etc/logrotate.d/gpslogd | grep "rotate [[:digit:]]" | sed 's/[^0-9]*//g')
+
+	# Check for the last rotated log.
+	last_log=$(find "${log_path}" -name 'gps.json.[[:digit:]].gz' | sort -n | tail -1)
+	last_log_count=$(echo $last_log | sed 's/[^0-9]*//g')
+
+	# If the last log is found the restart
+	log_count="${last_log_count}"
+	if [ "${last_log_count}" == "${number_of_logs}" ]; then
+		log_count="1"
+	else
+		log_count=$(("${log_count}" + 1))
+	fi
+
+	gzip "${original_log_file}"
+	mv "${original_log_file}".gz "${original_log_file}.${log_count}.gz"
 fi
 
-exec /usr/bin/gpspipe -w -o /var/log/gps.json localhost:2947
+exec /usr/bin/gpspipe -r -o "${original_log_file}" localhost:2947
 
